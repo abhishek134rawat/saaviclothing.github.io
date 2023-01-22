af3151930c11ecf1474e4e37a444bb159557898b
jerry <57862062+abhishek134rawat@users.noreply.github.com>
2023-01-22 17:25:19 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,37 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="tel:7838703651"><i class="fas fa-phone"></i> +91 7838703651</a>
+         <a href="mailto:abhishekrawat988@gmail.com"><i class="fas fa-envelope"></i> abhishekrawat@gmail.com</a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i> Delhi, India - 110067 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Abhishek rawat</span> | all rights reserved!</div>
+
+</footer>
